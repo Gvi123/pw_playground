e64f3594993a9417564c4acdd91228e477687cd5
Gvi123 <dambrauskas.gvidas@gmail.com>
2024-04-18 12:38:47 +0300

    added first assertion

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,4 +3,7 @@ const { test, expect } = require("@playwright/test");
 
 test("Sign in", async ({ page }) => {
   await page.goto("https://www.automationexercise.com/");
+  await expect(
+    page.getByRole("link", { name: "Website for automation" })
+  ).toBeVisible();
 });
